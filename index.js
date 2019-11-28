ddcd08d95cba6b4202125209eab6729d83d607ba
ezrankayamba <ezrankayamba@gmail.com>
2019-11-28 12:18:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const path = require("path");
+const handlebars = require("express-handlebars");
+const WebSocket = require("ws");
+const http = require("http");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || "8080";
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+let random = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+let list = [];
+["Juma Kapya", "Amos Tanu", "Hamis Rashid"].forEach((name, id) => {
+    list.push({
+        id: id,
+        name: name,
+        age: random(20, 61),
+        projects: random(1, 101),
+        performance: random(1, 101)
+    });
+});
+
+wss.on("connection", ws => {
+    ws.on("message", message => {
+        console.log(`Received message => ${message}`);
+        let data = JSON.parse(JSON.parse(message));
+        console.log(data);
+        if (data.cmd === "broadcast") {
+            let dist = null;
+            list.forEach((d, id) => {
+                if (id === parseInt(data.id)) {
+                    dist = d;
+                }
+            });
+            ws.broadcast(JSON.stringify(dist));
+            console.log("Broadcast sent");
+        } else {
+            console.log("Unknown command: ", data.cmd, data);
+        }
+    });
+    // ws.send("ho!");
+    ws.broadcast = data => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    };
+    wsRef = ws;
+});
+app.set("view engine", "hbs");
+
+app.engine(
+    "hbs",
+    handlebars({
+        layoutsDir: __dirname + "/views/layouts",
+        extname: "hbs"
+    })
+);
+
+app.use(express.json());
+// app.use(bodyParser);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("main", { layout: "index" });
+});
+
+app.get("/admin", (req, res) => {
+    res.render("admin", { layout: "index", distributors: list });
+});
+
+server.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,44 @@
+const url = "ws://localhost:8080";
+const connection = new WebSocket(url);
+(function() {
+    let path = window.location.pathname;
+    console.log("App started!", path);
+    if (path === "/admin") {
+        console.log("Admin page");
+        document.querySelectorAll(".btn-broadcast").forEach(el => {
+            el.addEventListener("click", e => {
+                let btn = e.target;
+                let id = btn.dataset.id;
+                let data = JSON.stringify({
+                    cmd: "broadcast",
+                    id: id
+                });
+                console.log(data);
+                connection.send(JSON.stringify(data));
+            });
+        });
+    } else {
+        console.log("Other users");
+        connection.onopen = () => {
+            console.log("Connected");
+        };
+        connection.onmessage = e => {
+            console.log(e.data);
+            document.querySelector("#distributor").classList.remove("hide");
+            let data = JSON.parse(JSON.parse(e.data));
+            console.log(data);
+            let setValue = (cls, value) => {
+                document.querySelector(`.${cls}`).innerHTML = value;
+            };
+            setValue("card-title", data.name);
+            setValue("card-subtitle", `Aged: ${data.age}`);
+            setValue(
+                "card-text",
+                `This distributor executed his duties for ${data.projects} and managed to archieve a performance of ${data.performance}%`
+            );
+        };
+        connection.onerror = error => {
+            console.log(`WebSocket error: ${error}`);
+        };
+    }
+})();
